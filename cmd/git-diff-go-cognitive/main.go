83eba119ab1bea5b8d1e2737fc90dde70983c42d
distroy <distroy@163.com>
2022-06-02 19:05:33 +0800

    cmd/git-diff-go-cognitive: draft

--- cmd/git-diff-go-cognitive/main.go
+++ cmd/git-diff-go-cognitive/main.go
@@ -4,5 +4,37 @@
 
 package main
 
+import (
+	"flag"
+
+	"github.com/distroy/git-go-tool/core/filter"
+	"github.com/distroy/git-go-tool/core/regexpcore"
+)
+
+type Flags struct {
+	Over   int
+	Top    int
+	Filter *filter.Filter
+}
+
+func paresFlags() *Flags {
+	f := &Flags{
+		Filter: &filter.Filter{
+			Includes: regexpcore.MustNewRegExps(nil),
+			Excludes: regexpcore.MustNewRegExps(regexpcore.DefaultExcludes),
+		},
+	}
+
+	flag.IntVar(&f.Over, "over", 0, "show functions with complexity > N only")
+	flag.IntVar(&f.Top, "top", 10, "show the top N most complex functions only")
+
+	flag.Var(f.Filter.Includes, "include", "the regexp for include pathes")
+	flag.Var(f.Filter.Excludes, "exclude", "the regexp for exclude pathes")
+
+	flag.Parse()
+
+	return f
+}
+
 func main() {
 }
---
+++ core/regexpcore/regexps.go
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) distroy
+ */
+
+package regexpcore
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var DefaultExcludes = []string{
+	`^vendor/`,
+	`/vendor/`,
+	`\.pb\.go$`,
+}
+
+type RegExps struct {
+	regexps []*regexp.Regexp
+	strings []string
+}
+
+func MustNewRegExps(exps []string) *RegExps {
+	res, _ := NewRegExps(exps)
+	if res == nil {
+		res = &RegExps{}
+	}
+	return res
+}
+
+func NewRegExps(exps []string) (*RegExps, error) {
+	res := &RegExps{
+		regexps: make([]*regexp.Regexp, 0, len(exps)),
+		strings: make([]string, 0, len(exps)),
+	}
+
+	var err error
+
+	for _, exp := range exps {
+		e := res.Set(exp)
+		if e != nil && err == nil {
+			err = e
+		}
+	}
+
+	return res, err
+}
+
+func (p *RegExps) Set(s string) error {
+	re, err := regexp.Compile(s)
+	if err != nil {
+		return err
+	}
+
+	p.regexps = append(p.regexps, re)
+	p.strings = append(p.strings, s)
+	return nil
+}
+
+func (p *RegExps) String() string {
+	return fmt.Sprint(p.strings)
+}
+
+func (p *RegExps) Check(s string) bool {
+	for _, re := range p.regexps {
+		loc := re.FindStringIndex(s)
+		if len(loc) == 2 {
+			return true
+		}
+	}
+	return false
+}
