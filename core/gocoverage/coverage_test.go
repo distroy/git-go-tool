213f6df15ca143fce77c27e8130b2056c8784c23
distroy <001distroy@gmail.com>
2022-06-04 02:04:33 +0800

    core/gocoverage: unit test

--- core/gocoverage/coverage_test.go
+++ core/gocoverage/coverage_test.go
@@ -5,9 +5,60 @@
 package gocoverage
 
 import (
+	"bytes"
+	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func Test_parseReader(t *testing.T) {
+	type args struct {
+		prefix string
+		reader io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Coverage
+		wantErr bool
+	}{
+		{
+			args: args{
+				prefix: "github.com/distroy/git-go-tool",
+				reader: bytes.NewBufferString(strings.Join([]string{
+					`mode: set`,
+					`github.com/distroy/git-go-tool/cmd/git-diff-go-cognitive/main.go:29.26,46.20 8 0`,
+					`github.com/distroy/git-go-tool/cmd/git-diff-go-cognitive/main.go:49.2,49.16 1 1`,
+					`github.com/distroy/git-go-tool/cmd/git-diff-go-cognitive/main.go:53.2,53.10 1 0`,
+					`github.com/distroy/git-go-tool/core/iocore/line_reader.go:84.2,84.33 1 1`,
+					`github.com/distroy/git-go-tool/core/iocore/line_reader.go:89.2,89.19 1 0`,
+					`github.com/distroy/git-go-tool/core/iocore/line_reader.go:80.16,82.3 1 1`,
+				}, "\n")),
+			},
+			want: []Coverage{
+				{Filename: "cmd/git-diff-go-cognitive/main.go", BeginLine: 29, EndLine: 46, Count: 0},
+				{Filename: "cmd/git-diff-go-cognitive/main.go", BeginLine: 49, EndLine: 49, Count: 1},
+				{Filename: "cmd/git-diff-go-cognitive/main.go", BeginLine: 53, EndLine: 53, Count: 0},
+				{Filename: "core/iocore/line_reader.go", BeginLine: 84, EndLine: 84, Count: 1},
+				{Filename: "core/iocore/line_reader.go", BeginLine: 89, EndLine: 89, Count: 0},
+				{Filename: "core/iocore/line_reader.go", BeginLine: 80, EndLine: 82, Count: 1},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseReader(tt.args.prefix, tt.args.reader)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseReader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseReader() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func Test_parseLine(t *testing.T) {
 	type args struct {
@@ -23,7 +74,7 @@ func Test_parseLine(t *testing.T) {
 		{
 			name: "mode line",
 			args: args{
-				prefix: "github.com/distroy/git-go-tool/",
+				prefix: "github.com/distroy/git-go-tool",
 				line:   "mode: set",
 			},
 			want:  nil,
@@ -32,7 +83,7 @@ func Test_parseLine(t *testing.T) {
 		{
 			name: "coverage",
 			args: args{
-				prefix: "github.com/distroy/git-go-tool/",
+				prefix: "github.com/distroy/git-go-tool",
 				line:   "github.com/distroy/git-go-tool/cmd/git-diff-go-cognitive/main.go:83.22,86.3 2 1",
 			},
 			want:  &Coverage{Filename: "cmd/git-diff-go-cognitive/main.go", BeginLine: 83, EndLine: 86, Count: 1},
