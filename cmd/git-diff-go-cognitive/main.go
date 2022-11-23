1cd59d00938a6483ce321cfc482dd96c0df3996d
distroy <distroy@163.com>
2022-11-23 15:37:35 +0800

    doc(cmd/git-diff-go-cognitive): fix flag usage

--- cmd/git-diff-go-cognitive/main.go
+++ cmd/git-diff-go-cognitive/main.go
@@ -26,7 +26,7 @@ const (
 
 type Flags struct {
 	ModeConfig modeservice.Config
-	Over       int `flag:"name:over; meta:N; default:15; usage:show functions with complexity > <N> only and return exit code 1 if the set is non-empty"`
+	Over       int `flag:"name:over; meta:N; default:15; usage:show functions with complexity <N> only and return exit code 1 if the set is non-empty"`
 	Top        int `flag:"name:top; meta:N; default:10; usage:show the top <N> most complex functions only"`
 	Filter     *filter.Filter
 }
