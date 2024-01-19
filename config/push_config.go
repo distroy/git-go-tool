134d2e3936abe96dca70481aadfa11b09e545bcd
distroy <distroy@163.com>
2024-01-19 17:22:59 +0800

    feat(config): fix push config

--- config/push_config.go
+++ config/push_config.go
@@ -7,8 +7,8 @@ package config
 var DefaultPush = &PushConfig{}
 
 type PushConfig struct {
-	PushUrl      string `yaml:"push_url"`
-	ProjectUrl   string `yaml:"project_url"` // env: CI_MERGE_REQUEST_PROJECT_URL
+	PushUrl      string `yaml:"push-url"`
+	ProjectUrl   string `yaml:"project-url"` // env: CI_MERGE_REQUEST_PROJECT_URL
 	TargetBranch string `yaml:"-"`           // env: CI_MERGE_REQUEST_TARGET_BRANCH_NAME
 	SourceBranch string `yaml:"-"`           // env: CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 }
